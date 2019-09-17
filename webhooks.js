d4090fb0521ca0fd9bab563b660a6ec67fdb0751
Page <page.didier@gmail.com>
2019-09-17 13:14:57 +0200

    <fix> recreate hmac for each request

--- webhooks.js
+++ webhooks.js
@@ -16,32 +16,32 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-const hmac = crypto.createHmac('sha1', secret)
 
 
 app.post('/deploy', (req, res) => {
-  const digest = 'sha1=' + hmac.update(JSON.stringify(req.body)).digest('hex')
-  const checksum = req.get(sigHeaderName);
+    const hmac = crypto.createHmac('sha1', secret);
+    const digest = 'sha1=' + hmac.update(JSON.stringify(req.body)).digest('hex')
+    const checksum = req.get(sigHeaderName);
 
-  if (!checksum || !digest || checksum !== digest){
-	res.send("Wrong signaure");
-  }
+    if (!checksum || !digest || checksum !== digest){
+        res.send("Wrong signaure");
+    }
 
-  const repoName = req.body.repository.name;
-  const path = repositoriesPath+repoName;
-  const branch = req.body.ref.split('/').pop();
+    const repoName = req.body.repository.name;
+    const path = repositoriesPath+repoName;
+    const branch = req.body.ref.split('/').pop();
 
-  exec('cd ' + path + ' && git pull',(error, stdout, stderr)=>{
-      console.log('stdout: ' + stdout);
-      if (error !== null) {
-          console.log('stderr: ' + stderr);
-          console.log('exec error: ' + error);
-      }
-      console.log("Done");	
-   });
+    exec('cd ' + path + ' && git pull',(error, stdout, stderr)=>{
+        console.log('stdout: ' + stdout);
+        if (error !== null) {
+            console.log('stderr: ' + stderr);
+            console.log('exec error: ' + error);
+        }
+        console.log("Done");
+    });
 
 
-   res.send('Done');
+    res.send('Done');
 
 });
 
