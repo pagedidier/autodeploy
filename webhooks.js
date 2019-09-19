d8f89f4fdb3c3e366203c0a942d9e7ae83270a22
Page <page.didier@gmail.com>
2019-09-19 19:21:08 +0200

    <feat><core> add deploy script handling

--- webhooks.js
+++ webhooks.js
@@ -1,4 +1,5 @@
 const configFile = require('./config.json');
+const scriptPath = "./deploy.sh";
 const config = configFile;
 const repositoriesPath = config.rootPath;
 const secret = config.secret;
@@ -8,6 +9,7 @@ let crypto = require('crypto');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const exec = require('child_process').exec;
 
@@ -31,18 +33,24 @@ app.post('/deploy', (req, res) => {
     const path = repositoriesPath+repoName;
     const branch = req.body.ref.split('/').pop();
 
-    exec('cd ' + path + ' && git pull',(error, stdout, stderr)=>{
+    let command ="git pull";
+    try {
+        if (fs.existsSync(scriptPath)) {
+            command = scriptPath;
+        }
+    } catch(err) {
+        console.error(err)
+    }
+
+    exec('cd ' + path + '&&' + command,(error, stdout, stderr)=>{
         console.log('stdout: ' + stdout);
         if (error !== null) {
             console.log('stderr: ' + stderr);
             console.log('exec error: ' + error);
         }
         console.log("Done");
+        res.json({'stdout': stdout , 'stderr': stderr});
     });
-
-
-    res.send('Done');
-
 });
 
 app.listen(3333);
